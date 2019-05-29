6a5b70ff5d1f79757da03c6d4262d472cc102309
lanxuexing_qt <lanxuexing_qt@he.chinamobile.com>
2019-05-30 01:35:41 +0800

    feat: combineLatest, withLatestFrom, zip

--- src/app/rxjs/study/demo06.component.ts
+++ src/app/rxjs/study/demo06.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Subscription, interval } from 'rxjs';
-import { take, combineLatest } from 'rxjs/operators';
+import { Subscription, interval, from } from 'rxjs';
+import { take, combineLatest, zip, withLatestFrom } from 'rxjs/operators';
 
 @Component({
     selector: 'app-rxjs-demo06',
@@ -36,6 +36,7 @@ export class RxjsDemo06Component implements OnInit, OnDestroy {
         /**
          * 取得各个observable 最后送出的值，再输出成一个值
          * combineLatest可以接收多个observable，最后一个参数是callback function，这个callback function接收的参数数量跟合并的observable数量相同
+         * callback 都会依照合并的observable 数量来传入参数，如果我们合并了三个observable，callback 就会有三个参数，而不管合并几个observable 都会只会回传一个值。
          * source:      ----0----1----2|
          * newest:      --0--1--2--3--4--5|
          *          combineLatest(newest, (x, y) => x + y);
@@ -51,9 +52,46 @@ export class RxjsDemo06Component implements OnInit, OnDestroy {
         });
     }
 
-    withLatestFromHandler() {}
+    withLatestFromHandler() {
+        /**
+         * 和combineLatest类似，但是withLatestFrom只有在主要的observable 送出新的值时，才会执行callback，附随的observable 只是在背景下运作
+         * withLatestFrom 会在main 送出值的时候执行callback，但请注意如果main 送出值时some 之前没有送出过任何值callback 仍然不会执行！
+         * callback 都会依照合并的observable 数量来传入参数，如果我们合并了三个observable，callback 就会有三个参数，而不管合并几个observable 都会只会回传一个值。
+         * main:       ----h----e----l----l----o|
+         * some:       --0--1--0--0--0--1|
+         *         withLatestFrom(some, (x, y) => y === 1 ? x.toUpperCase() : x);
+         * example:    ----h----e----l----L----O|
+         */
+        const main = from('hello').pipe(zip(interval(500), (x, y) => x));
+        const some = from([0, 1, 0, 0, 0, 1]).pipe(zip(interval(300), (x, y) => x));
+        const withLatestFromObservable = main.pipe(withLatestFrom(some, (x, y) => y === 1 ? x.toUpperCase() : x));
+        this.withLatestFromSubscription = withLatestFromObservable.subscribe({
+            next: (value) => { console.log('=====withLatestFrom操作符: ', value); },
+            error: (err) => { console.log('=====withLatestFrom操作符: Error: ', err); },
+            complete: () => { console.log('=====withLatestFrom操作符: complete!'); }
+        });
+    }
 
-    zipHandler() {}
+    zipHandler() {
+        /**
+         * 取每个observable 相同顺位的元素并传入callback，也就是说每个observable 的第n 个元素会一起被传入callback
+         * zip 会把各个observable 相同顺位送出的值传入callback
+         * zip 必须cache 住还没处理的元素，当我们两个observable 一个很快一个很慢时，就会cache 非常多的元素，等待比较慢的那个observable。这很有可能造成记忆体相关的问题！
+         * callback 都会依照合并的observable 数量来传入参数，如果我们合并了三个observable，callback 就会有三个参数，而不管合并几个observable 都会只会回传一个值。
+         * source:      ----0----1----2|
+         * newest:      --0--1--2--3--4--5|
+         *            zip(newest, (x, y) => x + y)
+         * exaple:      ----0----2----4|
+         */
+        const source = interval(500).pipe(take(3));
+        const newest = interval(300).pipe(take(6));
+        const zipObservable = source.pipe(zip(newest, (x, y) => x + y));
+        this.zipSubscription = zipObservable.subscribe({
+            next: (value) => { console.log('=====zip操作符: ', value); },
+            error: (err) => { console.log('=====zip操作符: Error: ', err); },
+            complete: () => { console.log('=====zip操作符: complete!'); }
+        });
+    }
 
     ngOnDestroy() {
         if (this.combineLatestSubscription) {
