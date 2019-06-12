e708c064201ffbd583f8a817b87761d903082e9f
兰学兴 <lanxuexing@xdf.cn>
2019-06-12 17:22:52 +0800

    style: rxjs demo02

--- src/app/rxjs/study/demo02.component.ts
+++ src/app/rxjs/study/demo02.component.ts
@@ -6,10 +6,26 @@ import { take } from 'rxjs/operators';
     selector: 'app-rxjs-demo02',
     template: `
         <h3>Rxjs Demo02 To Study! -- Observable的建立</h3>
-        <button id="fromEvent">FromEvent</button>
+        <button (click)="createHandler()">create</button>
+        <button class="mgLeft" (click)="ofHandler()">of</button>
+        <button class="mgLeft" (click)="fromHandler()">from</button>
+        <button class="mgLeft" (click)="fromEventHandler()">fromEvent</button>
+        <button class="mgLeft" (click)="fromEventPatternHandler()">fromEventPattern</button>
+        <button class="mgLeft" (click)="emptyHandler()">empty</button>
+        <button class="mgLeft" (click)="neverHandler()">never</button>
+        <button class="mgLeft" (click)="throwErrorHandler()">throwError</button>
+        <button class="mgLeft" (click)="coustomTimerHandler()">自定义定时器</button>
+        <button class="mgLeft" (click)="intervalHandler()">interval</button>
+        <button class="mgLeft" (click)="timerOfTimeHandler()">timer【时间】</button>
+        <button class="mgLeft" (click)="timerOfDateHandler()">timer【时期】</button>
+        <button class="mgLeft" id="btn">Button-Click</button>
         <app-back></app-back>
     `,
-    styles: [``]
+    styles: [`
+        .mgLeft {
+            margin-left: 20px;
+        }
+    `]
 })
 export class RxjsDemo02Component implements OnInit, OnDestroy {
     mySubscription: Subscription;
@@ -31,7 +47,17 @@ export class RxjsDemo02Component implements OnInit, OnDestroy {
     constructor() { }
 
     ngOnInit(): void {
-        // 1. Observable的创建
+        // timer Observable延时定时器, 延迟毫秒执行一次之后取消订阅
+        const onceTimerObservable = timer(3000);
+        this.onceTimerSubscription = onceTimerObservable.subscribe({
+            next: (value) => { console.log('======timer Observable延时定时器, 延迟毫秒执行一次之后取消订阅: ', value); },
+            error: (error) => { console.log('======timer Observable延时定时器, 延迟毫秒执行一次之后取消订阅---Error: ', error); },
+            complete: () => { console.log('======timer Observable延时定时器, 延迟毫秒执行一次之后取消订阅: complete'); }
+        });
+    }
+
+    createHandler() {
+        // Observable的创建
         const myObservable = Observable.create((observer) => {
             observer.next('Jerry');
             observer.next('Anna');
@@ -58,7 +84,9 @@ export class RxjsDemo02Component implements OnInit, OnDestroy {
         console.log('===2====start');
         this.mySubscription2 = myObservable.subscribe(innerObserver);
         console.log('===2====end');
+    }
 
+    ofHandler() {
         // 一次同步传递几个值
         const syncObservable = of('Jerry', 'Anna');
         this.syncSubscription = syncObservable.subscribe({
@@ -66,7 +94,9 @@ export class RxjsDemo02Component implements OnInit, OnDestroy {
             error: (error) => { console.log('=====一次同步传递值--Error: ', error); },
             complete: () => { console.log('=====一次同步传递值: complete'); }
         });
+    }
 
+    fromHandler() {
         // 从已经存在的阵列产生Observablè
         const existArray = ['Jerry', 'Anna', 2016, 2017, '30 days'];
         const existString = '铁人赛';
@@ -76,7 +106,6 @@ export class RxjsDemo02Component implements OnInit, OnDestroy {
             error: (error) => { console.log('======从已存在的阵列生成Observable---Error: ', error); },
             complete: () => { console.log('======从已存在的阵列生成Observable: complete'); }
         });
-
 
         // 来自Promise的Observable
         const promiseObservable = from(new Promise((resolve, reject) => {
@@ -89,15 +118,19 @@ export class RxjsDemo02Component implements OnInit, OnDestroy {
             error: (error) => { console.log('======来自Promise的Observable---Error: ', error); },
             complete: () => { console.log('======来自Promise的Observable: complete'); }
         });
+    }
 
+    fromEventHandler() {
         // FromEvent Observable
-        const fromEventObservable = fromEvent(document.getElementById('fromEvent'), 'click');
+        const fromEventObservable = fromEvent(document.getElementById('btn'), 'click');
         this.fromEventSubscription = fromEventObservable.subscribe({
             next: (value) => { console.log('======FromEvent Observable: ', value); },
             error: (error) => { console.log('======FromEvent Observable---Error: ', error); },
             complete: () => { console.log('======FromEvent Observable: complete'); }
         });
+    }
 
+    fromEventPatternHandler() {
         // FromEventPattern Observable
         const producer = new Producer();
         const fromEventPatternObservable = fromEventPattern(
@@ -110,7 +143,9 @@ export class RxjsDemo02Component implements OnInit, OnDestroy {
             complete: () => { console.log('======FromEventPattern Observable: complete'); }
         });
         producer.notify('Hello! Can you hear me?');
+    }
 
+    emptyHandler() {
         // empty Observable
         const emptyObservable = empty();
         this.emptySubscription = emptyObservable.subscribe({
@@ -118,7 +153,9 @@ export class RxjsDemo02Component implements OnInit, OnDestroy {
             error: (error) => { console.log('======empty Observable---Error: ', error); },
             complete: () => { console.log('======empty Observable: complete'); }
         });
+    }
 
+    neverHandler() {
         // never Observable
         const neverObservable = never();
         this.neverSubscription = neverObservable.subscribe({
@@ -126,7 +163,9 @@ export class RxjsDemo02Component implements OnInit, OnDestroy {
             error: (error) => { console.log('======never Observable---Error: ', error); },
             complete: () => { console.log('======never Observable: complete'); }
         });
+    }
 
+    throwErrorHandler() {
         // throwError Observable
         const throwErrorObservable = throwError('Oop!');
         this.throwErrorSubscription = throwErrorObservable.subscribe({
@@ -134,7 +173,9 @@ export class RxjsDemo02Component implements OnInit, OnDestroy {
             error: (error) => { console.log('======throwError Observable---Error: ', error); },
             complete: () => { console.log('======throwError Observable: complete'); }
         });
+    }
 
+    coustomTimerHandler() {
         // 自己实现一个定时器interval Observable
         const source = Observable.create(observer => {
             let i = 0;
@@ -150,7 +191,9 @@ export class RxjsDemo02Component implements OnInit, OnDestroy {
             error: (error) => { console.log('======自己实现一个定时器interval Observable---Error: ', error); },
             complete: () => { console.log('======自己实现一个定时器interval Observable: complete'); }
         });
+    }
 
+    intervalHandler() {
         // Rxjs版本的interval Observable
         const intervalObservable = interval(1000).pipe(take(4));
         this.intervalSubscription = intervalObservable.subscribe({
@@ -158,7 +201,9 @@ export class RxjsDemo02Component implements OnInit, OnDestroy {
             error: (error) => { console.log('======Rxjs版本的interval Observable---Error: ', error); },
             complete: () => { console.log('======Rxjs版本的interval Observable: complete'); }
         });
+    }
 
+    timerOfTimeHandler() {
         // timer Observable延时定时器
         const timerObservable = timer(1000, 5000).pipe(take(3));
         this.timerSubscription = timerObservable.subscribe({
@@ -166,21 +211,15 @@ export class RxjsDemo02Component implements OnInit, OnDestroy {
             error: (error) => { console.log('======timer Observable延时定时器---Error: ', error); },
             complete: () => { console.log('======timer Observable延时定时器: complete'); }
         });
+    }
 
+    timerOfDateHandler() {
         // timer Observable延时定时器【指定日期之后开始执行】
         const timerFromDateObservable = timer(new Date('2019-05-26 23:22:00')).pipe(take(1));
         this.timerFromDateSubscription = timerFromDateObservable.subscribe({
             next: (value) => { console.log('======timer Observable延时定时器【指定日期之后开始执行】: ', value); },
             error: (error) => { console.log('======timer Observable延时定时器【指定日期之后开始执行】---Error: ', error); },
             complete: () => { console.log('======timer Observable延时定时器【指定日期之后开始执行】: complete'); }
-        });
-
-        // timer Observable延时定时器, 延迟毫秒执行一次之后取消订阅
-        const onceTimerObservable = timer(3000);
-        this.onceTimerSubscription = onceTimerObservable.subscribe({
-            next: (value) => { console.log('======timer Observable延时定时器, 延迟毫秒执行一次之后取消订阅: ', value); },
-            error: (error) => { console.log('======timer Observable延时定时器, 延迟毫秒执行一次之后取消订阅---Error: ', error); },
-            complete: () => { console.log('======timer Observable延时定时器, 延迟毫秒执行一次之后取消订阅: complete'); }
         });
     }
 
