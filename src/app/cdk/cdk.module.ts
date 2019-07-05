4263a9066f84653b13a12de7e4b9f3f74c2784bd
兰学兴 <lanxuexing@xdf.cn>
2019-07-05 15:14:07 +0800

    chore: angular8 to angular7

--- src/app/cdk/cdk.module.ts
+++ src/app/cdk/cdk.module.ts
@@ -1,11 +1,13 @@
+import { A11yModule } from '@angular/cdk/a11y';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CoreModule } from '../core/core.module';
+import { AccessibilityComponent } from './accessibility/accessibility.component';
 import { CdkComponent } from './cdk.component';
 import { CdkRoutingModule } from './cdk.routing';
 import { CdkService } from './cdk.service';
-import { AccessibilityComponent } from './accessibility/accessibility.component';
+
 
 const COMMON = [
     CdkComponent,
@@ -14,8 +16,14 @@ const COMMON = [
 
 @NgModule({
     declarations: [ ...COMMON ],
-    imports: [ CommonModule, FormsModule, CdkRoutingModule, CoreModule ],
-    exports: [...COMMON ],
+    imports: [
+        CommonModule,
+        FormsModule,
+        CdkRoutingModule,
+        CoreModule,
+        A11yModule
+    ],
+    exports: [ ...COMMON ],
     providers: [ CdkService ],
 })
 export class CdkModule {}
